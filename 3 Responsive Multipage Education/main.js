f56d4659eb6477a276fb19c2aa12850f4e550930
chujingxl <chujingxl@gmail.com>
2022-05-28 22:22:33 +0800

    Education 完成homepage的mobile phone模式，完成aboutpage的computer模式

--- 3 Responsive Multipage Education/main.js
+++ 3 Responsive Multipage Education/main.js
@@ -21,18 +21,20 @@ faqs.forEach(faq => {
   })
 })
 
-// Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  // when window width is >= 600px
-  breakpoints: {
-    600: {
-      slidesPerView: 2
-    }
-  }
-});
+// on mobile devices, show / hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+})
+
+closeBtn.addEventListener('click', () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+})
+
